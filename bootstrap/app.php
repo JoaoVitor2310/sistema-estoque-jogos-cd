bd0b42adab5213d106f02dac2b759e0672d0401d
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-01-29 15:13:40 -0300

    fix: csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,12 +17,14 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->validateCsrfTokens(except: [
-            'http://127.0.0.1:8000/*',
-            'http://localhost:8000/*',
-            'http://localhost:3001/*',
-            'http://localhost:170/*',
-        ]);
+        $middleware->validateCsrfTokens(except: ['*']);
+
+        // $middleware->validateCsrfTokens(except: [
+        //     'http://127.0.0.1:8000/*',
+        //     'http://localhost:8000/*',
+        //     'http://localhost:3001/*',
+        //     'http://localhost:170/*',
+        // ]);
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
