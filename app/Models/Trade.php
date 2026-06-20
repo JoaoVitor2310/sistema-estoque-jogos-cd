ea98826a91348912cbf9e35bcca30f449f2f5483
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2026-06-20 14:12:29 -0300

    feat: add supplier trade relation

--- app/Models/Trade.php
+++ app/Models/Trade.php
@@ -3,14 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Trade extends Model
 {
     protected $table = 'trades';
 
-    protected $fillable = ['title', 'rows'];
+    protected $fillable = ['supplier_id', 'title', 'rows'];
 
     protected $casts = [
         'rows' => 'array',
     ];
+
+    public function supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
